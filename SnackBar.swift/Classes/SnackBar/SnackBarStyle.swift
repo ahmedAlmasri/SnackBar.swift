64cbbab14d109895cb365e66495e590a73d09890
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-03-08 11:53:32 +0100

    Remove iOS 13 colors

--- SnackBar.swift/Classes/SnackBar/SnackBarStyle.swift
+++ SnackBar.swift/Classes/SnackBar/SnackBarStyle.swift
@@ -10,15 +10,15 @@ import UIKit
 public struct SnackBarStyle {
 	public init() { }
 	// Container
-	public var background: UIColor = .systemGray6
+	public var background: UIColor = .lightGray
 	var padding = 5
 	var inViewPadding = 20
 	// Label
-	public var textColor: UIColor = .label
-	public var font: UIFont = UIFont.systemFont(ofSize: 14)
+	public var textColor: UIColor = .black
+    public var font: UIFont = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 14))
 	var maxNumberOfLines: UInt = 2
 	// Action
 	public var actionTextColorAlpha: CGFloat = 0.5
-	public var actionFont: UIFont = UIFont.systemFont(ofSize: 17)
+	public var actionFont: UIFont = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 17))
 	public var actionTextColor: UIColor = .red
 }
